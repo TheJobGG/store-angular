dec62ff783e49c36bd88a86d82152c8287722e98
TheJobGG <jobglez80@gmail.com>
2023-11-11 22:46:58 -0600

    Add thanks page component

---
+++ src/app/pages/thanks/thanks.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-thanks',
+  templateUrl: './thanks.component.html',
+  styles: [
+  ]
+})
+export class ThanksComponent {
+
+}
