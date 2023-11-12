cf1eeca83d875fdcc500dcbf3d86b0f3283a6dd5
TheJobGG <jobglez80@gmail.com>
2023-11-12 17:59:06 -0600

    Set backendUrl

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   stripeTestKey: 'pk_test_51K3mjCChWdPzT23UrqT97zhHX5agcRLAz9VkJ3NOd6ztM4NIuYDNE0izI8H9LSajpmzimKe62jAAM7DvZ2zAzWjL00oR4DauP3',
   storeBaseUrl: 'https://fakestoreapi.com',
-  backendBaseUrl: 'http://localhost:4242'
+  backendBaseUrl: 'https://server-stripe-store.vercel.app'
 };
--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   stripeTestKey: 'pk_test_51K3mjCChWdPzT23UrqT97zhHX5agcRLAz9VkJ3NOd6ztM4NIuYDNE0izI8H9LSajpmzimKe62jAAM7DvZ2zAzWjL00oR4DauP3',
   storeBaseUrl: 'https://fakestoreapi.com',
-  backendBaseUrl: 'https://server-stripe-store.vercel.app/'
+  backendBaseUrl: 'https://server-stripe-store.vercel.app'
 };
 
 /*
