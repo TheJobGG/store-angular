3c16c7af034d723835a94b4eecba1fe8d33a9f4d
TheJobGG <jobglez80@gmail.com>
2023-11-11 22:42:41 -0600

    Add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,35 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { CartComponent } from './pages/cart/cart.component';
+import { ProductPage } from './pages/product/product.component';
+import { ThanksComponent } from './pages/thanks/thanks.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "home",
+    component: HomeComponent
+  },
+  {
+    path: "",
+    redirectTo: "home", pathMatch: "full"
+  },
+  {
+    path: "cart",
+    component: CartComponent
+  },
+  {
+    path: 'product/:id',
+    component: ProductPage,
+  },
+  {
+    path: 'thanks',
+    component: ThanksComponent
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {scrollPositionRestoration: "top"})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
