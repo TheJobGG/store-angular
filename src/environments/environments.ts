7ceb4e9f5992cf51c75d9b0257a419506a3f4156
TheJobGG <jobglez80@gmail.com>
2023-11-12 15:28:46 -0600

    Add environment variables

---
+++ src/environments/environments.ts
@@ -0,0 +1,19 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  stripeTestKey: 'pk_test_51K3mjCChWdPzT23UrqT97zhHX5agcRLAz9VkJ3NOd6ztM4NIuYDNE0izI8H9LSajpmzimKe62jAAM7DvZ2zAzWjL00oR4DauP3',
+  storeBaseUrl: 'https://fakestoreapi.com',
+  backendBaseUrl: 'https://server-stripe-store.vercel.app/'
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
